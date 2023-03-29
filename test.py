594a601c95b8b692d8e6da66c983e6d8e4fb6cf4
hkim <mfiacp@hanmail.net>
2023-03-29 21:59:32 +0900

    test

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-print("test")
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from dotenv import load_dotenv
+import os
+import openai
+
+
+load_dotenv()
+openai.api_key = os.environ.get("openai_api_key")
+
+chat = ChatOpenAI(temperature=0)
+messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="Who won the world series in 2020?"),
+    AIMessage(content="The Los Angeles Dodgers won the World Series in 2020."),
+    HumanMessage(content="Where was it played?"),
+]
+
+print(chat(messages))
